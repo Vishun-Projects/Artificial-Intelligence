5a19e19238c9f73c7b2c7e9accbaebe0a0ff49a6
GRC Dev <dev@local>
2025-08-16 15:27:54 +0000

    chore: reset repository; keep README only

--- apps/api/src/common/permissions.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common'
-
-export type RequiredPermission = { resource: string; action: string }
-export const PERMISSIONS_KEY = 'required_permissions'
-export const Permissions = (...perms: RequiredPermission[]) => SetMetadata(PERMISSIONS_KEY, perms)
